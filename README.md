0b6d4109be99b25640ba683021d74f8ce92a2f70
Steffen Trog <stu204766@mail.uni-kiel.de>
2019-10-10 21:51:29 +0200

    Added basic project structure and trim.py

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Blade Stitching
 
-Source code and more for Steffen Trog's bachelor thesis
+Source code and more for Steffen Trog's bachelor thesis.
+
+## Installation
+
+Clone the repo, e. g. using SSH:
+
+```bash
+git@git.zs.informatik.uni-kiel.de:stu204766/bladestitching.git
+virtualenv venv && venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Directory structure
+
+| Dir   | What's in there       |
+|-------|-----------------------|
+| `pre` | Preprocessing scripts |
---
+++ pre/trim.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import os
+import ffmpeg
+
+
+def main(args):
+
+    print('Processing', args.videofile, ', creating file', args.output)
+    stream = ffmpeg.input(args.videofile)
+
+    reset_frame = True
+    if args.start:
+        if args.end:
+            print('Shaving off first', args.start,
+                  'and last', args.end, 'seconds')
+            stream = ffmpeg.trim(stream, start=args.start, end=args.end)
+        else:
+            print('Shaving off first', args.start, 'seconds')
+            stream = ffmpeg.trim(stream, start=args.start)
+    else:
+        if args.end:
+            print('Shaving off last', args.end, 'seconds')
+            stream = ffmpeg.trim(stream, end=args.end)
+        else:
+            print('Converting video file')
+            reset_frame = False
+
+    if reset_frame:
+        stream = ffmpeg.setpts(stream, 'PTS-STARTPTS')
+
+    stream = ffmpeg.output(stream, args.output)
+
+    ffmpeg.run(stream)
+    print('Done.', args.output)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('videofile',
+                        help='Video file to trim')
+    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('-s', '--start', type=int,
+                        help='Start of trimmed video in seconds')
+    parser.add_argument('-e', '--end', type=int,
+                        help='End of trimmed video in seconds')
+
+    args = parser.parse_args()
+
+    if not args.output:
+        # split ext
+        filename, file_extension = os.path.splitext(args.videofile)
+        args.output = filename + '_trimmed' + file_extension
+
+    print(args)
+
+    main(args)
