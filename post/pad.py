46803551b5cc19567af02d46b4cc7140a9d80428
Steffen Trog <stu204766@mail.uni-kiel.de>
2019-11-18 21:39:56 +0100

    Save reference files, but is this smart?

--- post/pad.py
+++ post/pad.py
@@ -2,32 +2,49 @@
 # -*- coding: utf8 -*-
 
 import os
-
 import cv2 as cv
-
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
 
-def pad(imagedir, translations, output):
+def pad(imagedir, translations, output, reference):
     df = pd.read_csv(translations, index_col=0)
-    xs = df['x']
-    ys = df['y']
-    for file in df.index:
+    reasonable = df[abs(df['x']) < 800]
+    reasonable = reasonable[abs(reasonable['y']) < 800]
+    files = reasonable.index
+    xs = reasonable['x']
+    ys = reasonable['y']
+    M = (np.absolute(np.array(xs)) + np.absolute(np.array(ys))).max()
+
+    print(len(df.index), 'files supplied, only',
+          len(files), 'have reasonable values (< 800px L1-translation):')
+    print(files)
+    print('Reference padding is', M)
+    for file in tqdm(files):
         in_path = os.path.join(imagedir, file)
         out_path = os.path.join(output, file)
+        ref_path = os.path.join(reference, file)
 
         img = cv.imread(in_path)
+
+        ref = cv.copyMakeBorder(img, M, M, M, M, 0)
+        cv.imwrite(ref_path, ref)
+
         x = xs[file]
         y = ys[file]
 
-        top, bottom = (-y, 0) if y < 0 else (0, y)
-        left, right = (-x, 0) if x < 0 else (0, x)
+        # print('Next image is', x, 'pixels further right and',
+        #       y, 'pixels further down')
 
-        top, bottom, left, right = int(top), int(bottom), int(left), int(right)
+        top, bottom = M - y, M + y
+        left, right = M - x, M + x
 
-        img = cv.copyMakeBorder(img, top, bottom, left, right,  0)
+        # print('Padding of this image is therefore top =', top, '| bottom =',
+        #       bottom, '| left =', left, '| right =', right)
 
-        cv.imwrite(out_path, img)
+        out = cv.copyMakeBorder(img, top, bottom, left, right, 0)
+        cv.imwrite(out_path, out)
 
 
 if __name__ == '__main__':
@@ -35,9 +52,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('input', help='Image file or directory')
-    parser.add_argument('translations', help='Scaling factor')
-    parser.add_argument('-o', '--output', help='Output file or directory')
+    parser.add_argument('input', help='Input directory')
+    parser.add_argument(
+        'translations', help='CSV file containing x,y offsets between images')
+    parser.add_argument(
+        '-o', '--output', help='Output directory of padded files')
+    parser.add_argument('-r', '--reference',
+                        help='Output directory of reference images')
     args = parser.parse_args()
 
     is_dir = os.path.isdir(args.input)
@@ -45,5 +66,8 @@ if __name__ == '__main__':
     if not args.output:
         args.output = os.path.join(args.input, os.path.pardir, 'padded')
         os.makedirs(args.output, exist_ok=True)
+    if not args.reference:
+        args.reference = os.path.join(args.input, os.path.pardir, 'reference')
+        os.makedirs(args.reference, exist_ok=True)
 
-    pad(args.input, args.translations, args.output)
+    pad(args.input, args.translations, args.output, args.reference)
