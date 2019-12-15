4ef65a1fb0555b469144de65f10b86293586271b
Steffen Trog <stu204766@mail.uni-kiel.de>
2019-12-15 18:35:45 +0100

    Heavily improve hough.py

--- stitch/linedetect/hough.py
+++ stitch/linedetect/hough.py
@@ -5,8 +5,12 @@ import os
 
 import cv2 as cv
 import numpy as np
+import pandas as pd
 
-import linedetect.lineutils as ut
+from tqdm import tqdm
+
+import lineutils as ut
+from lineutils import r, t
 
 
 def nubBy(predicate, iterable):
@@ -18,31 +22,32 @@ def nubBy(predicate, iterable):
 
 
 def naiveNubPredicate(r, s, max_rho=20, max_theta=0.1):
-    [[rho_r, theta_r]] = r
-    [[rho_s, theta_s]] = s
+    rho_r, theta_r = r
+    rho_s, theta_s = s
     diff_t = abs(theta_r - theta_s)
-    similar = abs(rho_r - rho_s) < max_rho and diff_t < max_theta
-    similar_inverted = abs(
-        rho_r + rho_s) < max_rho and abs(diff_t - np.pi) < max_theta
+    similar = (abs(rho_r - rho_s) < max_rho
+               and diff_t < max_theta)
+    similar_inverted = (abs(rho_r + rho_s) < max_rho
+                        and abs(diff_t - np.pi) < max_theta)
     return similar or similar_inverted
 
 
 def naiveFilter(line, max_deviation):
-    [[_, theta]] = line
+    theta = t(line)
+    # print(theta < max_deviation or abs(np.pi - theta)
+    #       < max_deviation, 'for', ut.eq(*line))
     return theta < max_deviation or abs(np.pi - theta) < max_deviation
 
 
 def findCenters(lines):
-    groups = {}
-    for l in lines:
-        [[rho, theta]] = l
-        line = (rho, theta)
-        similar_lines = list(
-            filter(
-                lambda other: ut.are_lines_similar(line, other),
-                groups.keys()
-            )
-        )
+
+    groups = {}  # Check similarity with keys. Values are lists of similar lines
+
+    for line in lines:
+        similar_lines = list(filter(
+            lambda other: ut.are_lines_similar(line, other),
+            groups.keys()
+        ))
         count = len(similar_lines)
         if count == 0:
             groups[line] = [line]
@@ -51,83 +56,105 @@ def findCenters(lines):
             if count > 1:
                 print('Found multiple lines similar to', ut.eq(*line), 'of which the first will be used:',
                       *[ut.eq(*x) for x in similar_lines])
-    result = []
-    for key in groups:
-        group = np.array(groups[key])
-        result.append([group.mean(0)])
-    #     print(*groups[key], ' ===>>> ', *result[-1])
-    # print(len(result), '    xxxxxxxxxx')
-    return result
+
+    return [np.array(group).mean(0) for group in groups.values()]
 
 
-def hough(imagefile, output=None,
-          filterPredicate=None,
-          nubPredicate=None, center=False,
+def hough(imagefile, outputfile=None,
+          threshold=150,
           normalize=True,
-          verbose=False,
-          paint=True):
-    img = cv.imread(imagefile)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray, 50, 150, apertureSize=3)
-    lines = cv.HoughLines(edges, 1, np.pi/180/100, 150)
+          filterPredicate=None,
+          nubPredicate=None,
+          verbose=False):
 
-    res = []
-    if lines is not None:
+    # Read image file
+    img = cv.imread(imagefile)
+    # Turn into grayscale
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Detect edges using canny edge detection
+    edges = cv.Canny(gray, 50, 150, apertureSize=3)
+    # Hough transform
+    lines = cv.HoughLines(edges, 1, np.pi/180/100, threshold)
+
+    if lines is None:
+        print('No lines found in', imagefile, ' :(')
+    else:
+        # Unpack lists of single elements
+        lines = [l for [l] in lines]
+
+        # Normalize
         if normalize:
-            lines = [[ut.normalize(l)] for [l] in lines]
+            lines = [ut.normalize(l) for l in lines]
+
+        # Filter
         if filterPredicate is not None:
             lines = filter(filterPredicate, lines)
-        if center:
-            lines = findCenters(lines)
-        elif nubPredicate is not None:
+
+        # Center or nub
+        if nubPredicate is not None:
             lines = nubBy(nubPredicate, lines)
-        for line in lines:
-            for rho, theta in line:
-                res.append((rho, theta))
-                if verbose:
-                    print('Line', str(len(res)) + ':',
-                          rho, '= x * sin(', theta, ') + y * cos(', theta, ')')
-                if paint:
-                    a = np.cos(theta)
-                    b = np.sin(theta)
-                    x0 = a*rho
-                    y0 = b*rho
-                    x1 = int(x0 + 1000*(-b))
-                    y1 = int(y0 + 1000*(a))
-                    x2 = int(x0 - 1000*(-b))
-                    y2 = int(y0 - 1000*(a))
+        else:
+            lines = findCenters(lines)
 
-                    cv.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        # Log
+        if verbose:
+            for line in lines:
+                print(ut.eq(*line))
 
-    if output is not None:
-        cv.imwrite(output, img)
-    if len(res) < 2:
-        print('WARNING: number of lines is merely',
-              len(res), 'in image file', imagefile)
+        # Paint
+        if outputfile is not None:
+            for line in lines:
+                rho, theta = line
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+
+                cv.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+            # Write copy of image to separate file for visualization
+            cv.imwrite(outputfile, img)
+
+        line_count = len(lines)
+        if line_count < 2:
+            print('WARNING: number of lines is merely',
+                  line_count, 'in image file', imagefile)
 
     if verbose:
-        print('Applied Hough transform from', imagefile,
-              'to ' + output if output is not None else '')
-    return res
+        print('Applied Hough transform on', imagefile,
+              'to ' + outputfile if outputfile is not None else '')
+    return lines
 
 
-def hough_all(imagedir, output,
-              nubPredicate=None,
+def hough_all(imagedir, outputfile,
+              paint_output=None,
+              threshold=150,
               filterPredicate=None,
-              center=False,
-              verbose=False,
-              paint=True):
+              nubPredicate=None,
+              verbose=False):
 
-    for file in sorted(os.listdir(imagedir)):
+    res = []
+
+    for file in tqdm(sorted(os.listdir(imagedir))):
         imagefile = os.path.join(imagedir, file)
-        outputfile = os.path.join(output, file)
+        paint_outputfile = os.path.join(paint_output, file)
 
-        hough(imagefile, outputfile,
-              nubPredicate=nubPredicate,
-              filterPredicate=filterPredicate,
-              center=center,
-              verbose=verbose,
-              paint=paint)
+        lines = hough(imagefile, outputfile=paint_outputfile,
+                      threshold=threshold,
+                      filterPredicate=filterPredicate,
+                      nubPredicate=nubPredicate,
+                      verbose=verbose)
+
+        if lines is not None:
+            for line in lines:
+                res.append([imagedir, r(line), t(line)])
+
+    df = pd.DataFrame(res, columns=['file', 'rho', 'theta'])
+    df.to_csv(outputfile, index=False)
 
 
 if __name__ == '__main__':
@@ -135,12 +162,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('input', help='Image file or directory')
-    parser.add_argument('-o', '--output', help='Output file or directory')
-    parser.add_argument('-n', '--nub', action='store_true',
-                        help='Filter out similar lines')
-    parser.add_argument('-c', '--center', action='store_true',
-                        help='Use center of lines close to each other')
+    parser.add_argument('input',
+                        help='Image file or directory')
+    parser.add_argument('-p', '--paint',
+                        help='Output file or directory to draw lines')
+    parser.add_argument('-o', '--output',
+                        help='Aggregate translations to output csv if input is directory')
+    parser.add_argument('-s', '--strategy', default='center', choices=['center', 'nub'],
+                        help='Use center of lines close to each other or Filter out similar lines')
+    parser.add_argument('-t', '--threshold', type=int,
+                        help='Threshold to use for Hough transformation')
     parser.add_argument('-d', '--max-v-deviation', type=float,
                         help='Filter lines by their maximum deviation from the vertical line (good default: 0.5)')
     parser.add_argument('-v', '--verbose', action='store_true',
@@ -148,39 +179,27 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.center and args.nub:
-        print('Cannot use --center option together with --nub option')
-        exit()
-
-    is_dir = os.path.isdir(args.input)
-
-    if not args.output:
-        if is_dir:
-            args.output = os.path.join(args.input, os.path.pardir, 'hough')
-            os.makedirs(args.output, exist_ok=True)
-        else:
-            # split ext
-            filename, ext = os.path.splitext(args.input)
-            args.output = filename + '_hough' + ext
-
-    if is_dir:
+    if os.path.isdir(args.input):
+        if not args.output:
+            args.output = os.path.join(args.input, 'hough.csv')
         hough_all(args.input, args.output,
+                  paint_output=args.paint,
+                  threshold=args.threshold,
+                  filterPredicate=(
+                      lambda l: naiveFilter(l, args.max_v_deviation)
+                  ) if args.max_v_deviation is not None else None,
                   nubPredicate=naiveNubPredicate
-                  if args.nub
-                  else None,
-                  filterPredicate=(lambda l: naiveFilter(
-                      l, args.max_v_deviation))
-                  if args.max_v_deviation is not None
-                  else None,
-                  center=args.center,
+                  if args.strategy == 'nub' else None,
                   verbose=args.verbose)
     else:
-        hough(args.input, args.output,
-              nubPredicate=naiveNubPredicate
-              if args.nub
-              else None,
-              filterPredicate=(lambda l: naiveFilter(l, args.max_v_deviation))
-              if args.max_v_deviation is not None
-              else None,
-              center=args.center,
-              verbose=args.verbose)
+        lines = hough(args.input, outputfile=args.paint,
+                      threshold=args.threshold,
+                      filterPredicate=(
+                          lambda l: naiveFilter(l, args.max_v_deviation)
+                      ) if args.max_v_deviation is not None else None,
+                      nubPredicate=naiveNubPredicate
+                      if args.strategy == 'nub' else None,
+                      verbose=args.verbose)
+        print('RESULT')
+        for line in lines:
+            print(ut.eq(*line))
