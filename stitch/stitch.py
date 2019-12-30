0d667fdaeebf0811cd1423024d33a07785f44e16
Steffen Trog <stu204766@mail.uni-kiel.de>
2019-12-30 12:53:01 +0100

    Replace old stitching script with new version

--- stitch/stitch.py
+++ stitch/stitch.py
@@ -10,32 +10,49 @@ from tqdm import tqdm
 
 import hough as ld
 import lineutils as ut
+from lineutils import r, t, x, y
+
+blue = (255, 0, 0)
+green = (0, 255, 0)
+red = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
 
 
-def stitch(imagedir, output=None, center=True):
-    files = sorted(os.listdir(imagedir))
-    file_paths = [os.path.join(imagedir, file) for file in files]
-    print('Applying Hough transformations to input images ...')
-    lines_per_image = [ld.hough(file_path,
-                                nubPredicate=None
-                                if center
-                                else ld.naiveNubPredicate,
-                                center=center,
-                                filterPredicate=lambda l: ld.naiveFilter(
-                                    l, 0.5),
-                                paint=True,
-                                output=os.path.join(os.path.dirname(file_path),
-                                                    os.pardir,
-                                                    'hough',
-                                                    os.path.basename(file_path)))
-                       for file_path in tqdm(file_paths)]
-    line_files = [LineImage(p, l) for p, l in zip(file_paths, lines_per_image)]
-    pairs = list(zip(line_files, line_files[1:]))
+def stitch(imagedir, cachefile=None, output=None, center=True):
+
+    if cachefile is None:
+        # Compute Hough lines from scratch
+        print('Applying Hough transformations to input images ...')
+        files = sorted(os.listdir(imagedir))
+        file_paths = [os.path.join(imagedir, file) for file in files]
+        lines_per_image = [ld.hough(file_path,
+                                    outputfile=os.path.join(os.path.dirname(file_path),
+                                                            os.pardir,
+                                                            'hough',
+                                                            os.path.basename(file_path)),
+                                    filterPredicate=(
+                                        lambda l: ld.naiveFilter(l, 0.5)
+                                    ),
+                                    nubPredicate=(
+                                        None if center else ld.naiveNubPredicate
+                                    ))
+                           for file_path in tqdm(file_paths)]
+        line_files = [LineImage(p, l)
+                      for p, l in zip(file_paths, lines_per_image)]
+    else:
+        # Rely on cache (csv containing lines)
+        print('Reading Hough lines from cache ...')
+        df = pd.read_csv(cachefile)
+        lines_per_file = {file: list(zip(group_df['rho'], group_df['theta']))
+                          for file, group_df in df.groupby(by='file')}
+        line_files = [LineImage(os.path.join(imagedir, p), l)
+                      for p, l
+                      in sorted(lines_per_file.items(), key=lambda x:x[0])]
 
     translations = {}
 
-    for current_image, next_image in pairs:
-        img = cv.imread(current_image.img_path)
+    for current_image, next_image in zip(line_files, line_files[1:]):
 
         current_image.init_twins(next_image)
 
@@ -70,137 +87,69 @@ def stitch(imagedir, output=None, center=True):
         # rho, theta are simply x, y in polar coords
         for (c_l, n_l), (c_r, n_r) in twins:
 
-            # Current image's left, bisecting and right line
-            c_rho_l, c_theta_l = c_l
-            c_rho_b, c_theta_b = ut.get_bisecting_line(c_l, c_r)
-            c_rho_r, c_theta_r = c_r
+            # Compute bisecting lines
+            c_b = ut.get_bisecting_line(c_l, c_r)
+            n_b = ut.get_bisecting_line(n_l, n_r)
 
-            # Next image's left, bisecting and right line
-            n_rho_l, n_theta_l = n_l
-            n_rho_b, n_theta_b = ut.get_bisecting_line(n_l, n_r)
-            n_rho_r, n_theta_r = n_r
+            # Move this distance to align bisec foot points
+            x_diff_b, y_diff_b = x(n_b) - x(c_b), y(n_b) - y(c_b)
 
-            # Current image's line's foot points
-            c_x_l, c_y_l = (c_rho_l * np.cos(c_theta_l),
-                            c_rho_l * np.sin(c_theta_l))
-            c_x_b, c_y_b = (c_rho_b * np.cos(c_theta_b),
-                            c_rho_b * np.sin(c_theta_b))
-            c_x_r, c_y_r = (c_rho_r * np.cos(c_theta_r),
-                            c_rho_r * np.sin(c_theta_r))
+            # Use these two variables to track the overall vertical translation for each side
+            translate_y_l = y_diff_b
+            translate_y_r = y_diff_b
 
-            # Next image's line's foot points
-            n_x_l, n_y_l = (n_rho_l * np.cos(n_theta_l),
-                            n_rho_l * np.sin(n_theta_l))
-            n_x_b, n_y_b = (n_rho_b * np.cos(n_theta_b),
-                            n_rho_b * np.sin(n_theta_b))
-            n_x_r, n_y_r = (n_rho_r * np.cos(n_theta_r),
-                            n_rho_r * np.sin(n_theta_r))
+            # Move current lines
+            c_l = ut.translate(c_l, x_diff_b, y_diff_b)
+            c_b = ut.translate(c_b, x_diff_b, y_diff_b)
+            c_r = ut.translate(c_r, x_diff_b, y_diff_b)
 
-            translate_x_l = None
-            translate_y_l = None
+            # Foot points should now be "equal" (deviate less than 1 pixel) for the bisecting lines
+            bft_x, bft_y = x(n_b), y(n_b)  # = x(c_b), y(c_b)
 
-            translate_x_r = None
-            translate_y_r = None
+            # TODO: can we account for the following rotation
+            # in the final translation values?
 
-            # TODO: rotate everything according to c_b and n_b
-            # in order to avoid /0 error here
+            # Rotate current lines and next lines
+            # such that the bisection lines are both vertical
+            c_rotate, n_rotate = -t(c_b), -t(n_b)
+            c_l = ut.rotate(c_l, c_rotate, bft_x, bft_y)
+            c_b = ut.rotate(c_b, c_rotate, bft_x, bft_y)
+            c_r = ut.rotate(c_r, c_rotate, bft_x, bft_y)
+            n_l = ut.rotate(n_l, n_rotate, bft_x, bft_y)
+            n_b = ut.rotate(n_b, n_rotate, bft_x, bft_y)
+            n_r = ut.rotate(n_r, n_rotate, bft_x, bft_y)
 
-            if not abs(c_theta_l) < 1e-5:
-                x_diff_l = (n_x_l - c_x_l
-                            + n_x_b - c_x_b)
-                y_diff_l = (n_y_l - c_y_l
-                            + n_y_b - c_y_b)
+            # Compute how far both current lines
+            # need to be translated in vertical direction
+            # to match both next lines
+            translate_y_l += ut.vertical_distance(n_l, c_l)
+            translate_y_r += ut.vertical_distance(n_r, c_r)
 
-                theta_diff_l = (0.5 * np.pi
-                                - c_theta_l
-                                - np.arctan(y_diff_l
-                                            / x_diff_l))
+            # The only time we translated horizontally was in the beginning, so we just copy that value
+            translate_x = x_diff_b
+            # Take the average over both translation for left and right
+            translate_y = 0.5 * (translate_y_l + translate_y_r)
 
-                bottom_len_l = np.sqrt(x_diff_l * x_diff_l
-                                       + y_diff_l * y_diff_l)
-                up_len_l = (bottom_len_l
-                            * np.sin(theta_diff_l)
-                            / np.sin(c_theta_l))
-
-                translate_x_l = x_diff_l
-                translate_y_l = y_diff_l + up_len_l
-                # cv.line(img,
-                #         (100, 400),
-                #         (int(100+translate_x_l), int(400 + translate_y_l)),
-                #         (0, 0, 0),
-                #         thickness=1)
-
-            if not abs(c_theta_r) < 1e-5:
-                x_diff_r = (n_x_r - c_x_r
-                            + n_x_b - c_x_b)
-                y_diff_r = (n_y_r - c_y_r
-                            + n_y_b - c_y_b)
-
-                theta_diff_r = (0.5 * np.pi
-                                - c_theta_r
-                                - np.arctan(y_diff_r
-                                            / x_diff_r))
-
-                bottom_len_r = np.sqrt(x_diff_r * x_diff_r
-                                       + y_diff_r * y_diff_r)
-                up_len_r = (bottom_len_r
-                            * np.sin(theta_diff_r)
-                            / np.sin(c_theta_r))
-
-                translate_x_r = x_diff_r
-                translate_y_r = y_diff_r + up_len_r
-                # cv.line(img,
-                #         (300, 400),
-                #         (int(300+translate_x_r), int(400 + translate_y_r)),
-                #         (0, 0, 0),
-                #         thickness=1)
-            else:
-                translate_x_r = translate_x_l
-                translate_y_r = translate_y_l
-
-            if translate_x_l is None:
-                translate_x_l = translate_x_r
-                translate_y_l = translate_y_r
-
-            if translate_x_l is not None:
-                translate_x = 0.5 * (translate_x_l + translate_x_r)
-                translate_y = 0.5 * (translate_y_l + translate_y_r)
-                image_translations.append([translate_x, translate_y])
-                # cv.line(img,
-                #         (200, 400),
-                #         (200 + translate_x, 400 + translate_y),
-                #         (0, 0, 0),
-                #         thickness=3)
-
-                # cv.line(img,
-                #         (-1, 480 + translate_y),
-                #         (10000, 480 + translate_y),
-                #         (0, 0, 0),
-                #         thickness=1)
+            image_translations.append([translate_x, translate_y])
 
         if len(image_translations) > 0:
             translation = np.array(image_translations).mean(0).astype(int)
             key = os.path.basename(current_image.img_path)
             ref = os.path.basename(next_image.img_path)
             translations[key] = (ref, translation)
-            print(current_image.img_path, '-->',
-                  next_image.img_path, ':: moved by', translation)
-        p_dir = os.path.join(imagedir, os.path.pardir,
-                             'stitch-c' if center else 'stitch-n')
-        os.makedirs(p_dir, exist_ok=True)
-        p = os.path.join(p_dir, os.path.basename(current_image.img_path))
-        cv.imwrite(p, img)
 
+    paths = list(sorted(translations.keys()))
+    refs = [translations[p][0] for p in paths]
+    xs = [translations[p][1][0] for p in paths]
+    ys = [translations[p][1][1] for p in paths]
+    df = pd.DataFrame({'ref': refs, 'x': xs, 'y': ys}, index=paths)
     if output is not None:
-        paths = list(sorted(translations.keys()))
-        refs = [translations[p][0] for p in paths]
-        xs = [translations[p][1][0] for p in paths]
-        ys = [translations[p][1][1] for p in paths]
-        df = pd.DataFrame({'ref': refs, 'x': xs, 'y': ys}, index=paths)
-        print('#####')
-        print(df)
-        print('#####')
+        print('Done.', output)
         df.to_csv(output)
+    else:
+        print('Done. Result:')
+        print(df)
+        print('Results not written to disk as output file was not specified.')
 
 
 class LineImage:
@@ -233,33 +182,25 @@ class LineImage:
                         lambda l: ld.ut.are_lines_similar(l, line),
                         image.lines
                     ),
-                    key=lambda l: l[0] - line[0]
+                    # similarity heuristic: compare distances of foot points from origin
+                    key=lambda l: abs(r(line) - r(l))
                 )
             )
             if(len(neighbors) > 0):
+                twin = neighbors[0]
                 if(len(neighbors) > 1):
-                    print('WARNING: Ignoring other similar line(s)!',
-                          [ut.eq(l) for l in neighbors[1:]])
-                self.twins[line] = neighbors[0]
+                    print('WARNING: Ignoring other similar line(s) of',
+                          ut.eq(line), 'besides', ut.eq(twin) + '!', '(', self.img_path, ')')
+                    print([ut.eq(l) for l in neighbors[1:]])
+                self.twins[line] = twin
             else:
                 print('WARNING: Line cannot be found in next image!',
-                      ut.eq(line))
+                      ut.eq(line), '(', self.img_path, ')')
 
     def __str__(self):
-        return str(self.img_path) + ' has lines ' + str(
-            self.lines) + ' which correspond to ' + str(self.twins)
-
-# def draw_line(img, rho, theta, color=(0, 0, 255), width=2):
-#     a = np.cos(theta)
-#     b = np.sin(theta)
-#     x0 = a * rho
-#     y0 = b * rho
-#     x1 = int(x0 + b * -1000)
-#     y1 = int(y0 + a * 1000)
-#     x2 = int(x0 - b * -1000)
-#     y2 = int(y0 - a * 1000)
-
-#     cv.line(img, (x1, y1), (x2, y2), color, width)
+        return (str(self.img_path)
+                + ' has lines ' + str(self.lines)
+                + ' which correspond to ' + str(self.twins))
 
 
 if __name__ == '__main__':
@@ -267,11 +208,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('input', help='Image directory')
+    parser.add_argument('input',
+                        help='Image directory')
     parser.add_argument('strategy', choices=['nub', 'center'],
                         help='Decide whether adjacent lines should be discarded or joined')
-    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('-c', '--cache',
+                        help='Cache file containing precomputed hough lines')
+    parser.add_argument('-o', '--output',
+                        help='Output file')
 
     args = parser.parse_args()
 
-    stitch(args.input, output=args.output, center=args.strategy == 'center')
+    stitch(args.input,
+           cachefile=args.cache,
+           output=args.output,
+           center=args.strategy == 'center')
