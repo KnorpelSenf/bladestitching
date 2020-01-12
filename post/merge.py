009212fcc8db31881d0fea776922650bd56896fd
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-01-12 17:51:14 +0100

    Fix and refactor merge script

--- post/merge.py
+++ post/merge.py
@@ -10,27 +10,27 @@ def merge(offsetsfile, imagedir, outputfile):
     df = pd.read_csv(offsetsfile, index_col=0)
     d = df.to_dict(orient='index')
 
-    current = min(d.keys())
-    img = cv.imread(os.path.join(imagedir, current))
+    file_img = min(d.keys())
+    img = cv.imread(os.path.join(imagedir, file_img))
     current_x, current_y = 0, 0
 
-    while current in d:
-        c = d[current]
+    while file_img in d:
+        c = d[file_img]
         x = c['x']
         y = c['y']
 
-        ref = c['ref']
-        ref_path = os.path.join(imagedir, ref)
-        ref_img = cv.imread(ref_path)
+        file_ref = c['ref']
+        path_ref = os.path.join(imagedir, file_ref)
+        img_ref = cv.imread(path_ref)
 
-        print(current, '->', ref, '|', x, y)
+        print(file_img, '->', file_ref, '|', x, y)
 
         # w, h of current image
-        w_img, h_img = img.shape[0], img.shape[1]
+        w_img, h_img = img.shape[1], img.shape[0]
 
         # relative x, relative y, w, h of next image
         x_ref, y_ref = current_x + x, current_y + y
-        w_ref, h_ref = ref_img.shape[0], ref_img.shape[1]
+        w_ref, h_ref = img_ref.shape[1], img_ref.shape[0]
 
         border_right = max(0, x_ref + w_ref - w_img)
         border_bottom = max(0, y_ref + h_ref - h_img)
@@ -50,11 +50,10 @@ def merge(offsetsfile, imagedir, outputfile):
         img = cv.copyMakeBorder(img,
                                 border_top, border_bottom, border_left, border_right,
                                 0)
-
-        img[x_ref:x_ref+w_ref, y_ref:y_ref+h_ref] = ref_img
+        img[y_ref:y_ref+h_ref, x_ref:x_ref+w_ref] = img_ref
 
         current_x, current_y = x_ref, y_ref
-        current = ref
+        file_img = file_ref
 
     cv.imwrite(outputfile, img)
     print('Done.', os.path.abspath(outputfile))
