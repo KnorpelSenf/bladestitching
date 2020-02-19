590b05acb50fa579daeb462beaff0ad57af12ae5
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-19 19:56:55 +0100

    We get an iterative approach for free

--- stitch/stitch.py
+++ stitch/stitch.py
@@ -5,6 +5,7 @@ import os
 
 import cv2 as cv
 import numpy as np
+from scipy import optimize as so
 import pandas as pd
 from tqdm import tqdm
 
@@ -19,7 +20,7 @@ def fst(x): return x[0]
 def snd(x): return x[1]
 
 
-def stitch(imagedir, image_height, cachefile, l1radius=50, output=None):
+def stitch(method, imagedir, image_height, cachefile, l1radius=50, output=None):
 
     print('Reading Hough lines')
     df = pd.read_csv(cachefile)
@@ -58,91 +59,105 @@ def stitch(imagedir, image_height, cachefile, l1radius=50, output=None):
             )
         )
 
-        # we need two twins (that is, four lines) for each translation computation,
-        # so we generate all possible combinations of twins (pairs of twins)
-        count = len(line_pairs)
-        twin_combinations = [(line_pairs[l], line_pairs[r])
-                             for l in range(0, count)
-                             for r in range(l + 1, count)]
+        if method == 'analytical':
 
-        # translation values for each pair of twins
-        image_translations = []
+            # we need two twins (that is, four lines) for each translation computation,
+            # so we generate all possible combinations of twins (pairs of twins)
+            count = len(line_pairs)
+            twin_combinations = [(line_pairs[l], line_pairs[r])
+                                 for l in range(0, count)
+                                 for r in range(l + 1, count)]
 
-        # Naming conventions:
-        # Prefix c_ stands for C_urrent set of lines
-        # Prefix n_ stands for N_ext set of lines
-        # Postfix _l stands for _Left line
-        # Postfix _b stands for _Bisection line
-        # Postfix _r stands for _Right line
-        # x means x coord, y means y coord of foot point
-        # rho, theta are simply x, y in polar coords
+            # translation values for each pair of twins
+            image_translations = []
 
-        #   left twin   right twin
-        for (c_l, n_l), (c_r, n_r) in twin_combinations:
+            # Naming conventions:
+            # Prefix c_ stands for C_urrent set of lines
+            # Prefix n_ stands for N_ext set of lines
+            # Postfix _l stands for _Left line
+            # Postfix _b stands for _Bisection line
+            # Postfix _r stands for _Right line
+            # x means x coord, y means y coord of foot point
+            # rho, theta are simply x, y in polar coords
 
-            # Compute bisecting lines
-            c_b = ut.get_bisecting_line(c_l, c_r)
-            n_b = ut.get_bisecting_line(n_l, n_r)
+            #   left twin   right twin
+            for (c_l, n_l), (c_r, n_r) in twin_combinations:
 
-            # Move this distance to align bisec foot points
-            x_diff_b, y_diff_b = x(n_b) - x(c_b), y(n_b) - y(c_b)
+                # Compute bisecting lines
+                c_b = ut.get_bisecting_line(c_l, c_r)
+                n_b = ut.get_bisecting_line(n_l, n_r)
 
-            # Use these two variables to track the overall vertical translation for each side
-            translate_y_l = y_diff_b
-            translate_y_r = y_diff_b
+                # Move this distance to align bisec foot points
+                x_diff_b, y_diff_b = x(n_b) - x(c_b), y(n_b) - y(c_b)
 
-            # Move current lines
-            c_l = ut.translate(c_l, x_diff_b, y_diff_b)
-            c_b = ut.translate(c_b, x_diff_b, y_diff_b)
-            c_r = ut.translate(c_r, x_diff_b, y_diff_b)
+                # Use these two variables to track the overall vertical translation for each side
+                translate_y_l = y_diff_b
+                translate_y_r = y_diff_b
 
-            # Foot points should now be "equal" (deviate less than 1 pixel) for the bisecting lines
-            bft_x, bft_y = x(n_b), y(n_b)  # = x(c_b), y(c_b)
+                # Move current lines
+                c_l = ut.translate(c_l, x_diff_b, y_diff_b)
+                c_b = ut.translate(c_b, x_diff_b, y_diff_b)
+                c_r = ut.translate(c_r, x_diff_b, y_diff_b)
 
-            # Note how we never account for the following rotation
-            # in the final translation values, see stitchrelative.py
-            # for a script that does this (and produces worse results!)
+                # Foot points should now be "equal" (deviate less than 1 pixel) for the bisecting lines
+                bft_x, bft_y = x(n_b), y(n_b)  # = x(c_b), y(c_b)
 
-            # Rotate current lines and next lines
-            # such that the bisection lines are both vertical
-            c_rotate, n_rotate = -t(c_b), -t(n_b)
-            c_l = ut.rotate(c_l, c_rotate, bft_x, bft_y)
-            c_b = ut.rotate(c_b, c_rotate, bft_x, bft_y)
-            c_r = ut.rotate(c_r, c_rotate, bft_x, bft_y)
-            n_l = ut.rotate(n_l, n_rotate, bft_x, bft_y)
-            n_b = ut.rotate(n_b, n_rotate, bft_x, bft_y)
-            n_r = ut.rotate(n_r, n_rotate, bft_x, bft_y)
+                # Note how we never account for the following rotation
+                # in the final translation values, see stitchrelative.py
+                # for a script that does this (and produces worse results!)
 
-            # Compute how far the current lines
-            # need to be translated in vertical direction
-            # to match the next lines
-            translate_y_l += ut.vertical_distance(n_l, c_l)
-            translate_y_r += ut.vertical_distance(n_r, c_r)
+                # Rotate current lines and next lines
+                # such that the bisection lines are both vertical
+                c_rotate, n_rotate = -t(c_b), -t(n_b)
+                c_l = ut.rotate(c_l, c_rotate, bft_x, bft_y)
+                c_b = ut.rotate(c_b, c_rotate, bft_x, bft_y)
+                c_r = ut.rotate(c_r, c_rotate, bft_x, bft_y)
+                n_l = ut.rotate(n_l, n_rotate, bft_x, bft_y)
+                n_b = ut.rotate(n_b, n_rotate, bft_x, bft_y)
+                n_r = ut.rotate(n_r, n_rotate, bft_x, bft_y)
 
-            # The only time we translated horizontally was in the beginning, so we just copy that value
-            translate_x = x_diff_b
-            # Take the average over both translation for left and right
-            translate_y = 0.5 * (translate_y_l + translate_y_r)
+                # Compute how far the current lines
+                # need to be translated in vertical direction
+                # to match the next lines
+                translate_y_l += ut.vertical_distance(n_l, c_l)
+                translate_y_r += ut.vertical_distance(n_r, c_r)
 
-            image_translations.append([translate_x, translate_y])
+                # The only time we translated horizontally was in the beginning, so we just copy that value
+                translate_x = x_diff_b
+                # Take the average over both translation for left and right
+                translate_y = 0.5 * (translate_y_l + translate_y_r)
 
-        if len(image_translations) > 0:
-            # average over all values and round to pixel accuracy
-            translation = np.rint(
-                np.array(image_translations)
-                .mean(0)
-            ).astype(int)
+                image_translations.append([translate_x, translate_y])
 
-            # Optimize result based on error function
-            translation = optimize_line_distances(
-                line_pairs, translation, image_height,
-                l1radius=l1radius
+            if len(image_translations) > 0:
+                # average over all values and round to pixel accuracy
+                translation = np.rint(
+                    np.array(image_translations)
+                    .mean(0)
+                ).astype(int)
+
+                if l1radius > 0:
+                    # Optimize result based on error function
+                    translation = optimize_line_distances(
+                        line_pairs, translation, image_height,
+                        l1radius=l1radius
+                    )
+            else:
+                translation = (0, 0)
+                print('WARNING:', 'Insufficient lines in analytical mode for image pair',
+                      current_image, next_image)
+
+        else:  # method == 'iterative'
+            res = so.minimize(
+                lambda t: compute_error(line_pairs, t, image_height),
+                (0, 0)
             )
+            translation = tuple(map(int, res.x))
 
-            # store reference image and translation value in result dict
-            key = os.path.basename(current_image.img_path)
-            ref = os.path.basename(next_image.img_path)
-            translations[key] = (ref, translation)
+        # store reference image and translation value in result dict
+        key = os.path.basename(current_image.img_path)
+        ref = os.path.basename(next_image.img_path)
+        translations[key] = (ref, translation)
 
     paths = list(sorted(translations.keys()))
     refs = [translations[p][0] for p in paths]
@@ -267,6 +282,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+    parser.add_argument('method', choices=['analytical', 'iterative'],
+                        help='Perform stitching in an analytical or iterative manner')
     parser.add_argument('hough',
                         help='File containing precomputed hough lines')
     parser.add_argument('input',
@@ -280,6 +297,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    stitch(args.input, args.height, args.hough,
+    stitch(args.method, args.input, args.height, args.hough,
            l1radius=args.local_optimization,
            output=args.output)
