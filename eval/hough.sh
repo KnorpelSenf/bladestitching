ca138da2012f163fde7593be956cee3d43635b18
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-22 16:16:10 +0100

    Use proper threshold

--- eval/hough.sh
+++ eval/hough.sh
@@ -10,8 +10,8 @@ for strategy in {center,nub}; do
             P=data/processed/dec19/kalkar1/thermal/eval/$strategy/$threshold/$maxvdev
             mkdir $P $P/hough
 
-            echo "Strategy: $strategy | Threshold: $threshold | Max dev: $maxvdev >>> $P"
-            ./stitch/hough.py data/processed/dec19/kalkar1/thermal/data/ -p $P/paint -o $P/hough.csv -s $strategy -t $threshold -d $maxvdev --max-workers $(nproc) > $P/log.txt
+            echo "Strategy: $strategy | Threshold: $threshold | Max dev: 0.$maxvdev >>> $P"
+            ./stitch/hough.py data/processed/dec19/kalkar1/thermal/data/ -p $P/paint -o $P/hough.csv -s $strategy -t $threshold -d 0.$maxvdev --max-workers $(nproc) > $P/log.txt
 
         done
     done
