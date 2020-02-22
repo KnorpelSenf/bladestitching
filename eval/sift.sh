d4bc6e7828a8a5743e01090805760791d917af94
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-22 17:15:12 +0100

    More eval scripts

---
+++ eval/sift.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir data/processed/dec19/kalkar1/thermal/eval/sift
+
+for count in {0,10,20,50,100,200,500,1000,2000,5000,10000}; do
+    mkdir data/processed/dec19/kalkar1/thermal/eval/sift/$count
+
+    for ct in {1..9}; do
+        mkdir data/processed/dec19/kalkar1/thermal/eval/sift/$count/$ct
+
+        for et in {2..18..2}; do
+            P=data/processed/dec19/kalkar1/thermal/eval/sift/$count/$ct/$et
+            mkdir $P $P/sift
+
+            echo "Count: $count | CT: $ct | ET: $et >>> $P"
+            ./stitch/hough.py data/processed/dec19/kalkar1/thermal/data/ -o $P/sift -c $count -q $ct -d $et > $P/log.txt
+
+        done
+    done
+done
