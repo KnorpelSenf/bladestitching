6d8c9c0314e8682dd31482f1836743dd21c6f664
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-22 16:50:44 +0100

    Fix output not being written

--- eval/hough.sh
+++ eval/hough.sh
@@ -11,7 +11,7 @@ for strategy in {center,nub}; do
             mkdir $P $P/hough
 
             echo "Strategy: $strategy | Threshold: $threshold | Max dev: 0.$maxvdev >>> $P"
-            ./stitch/hough.py data/processed/dec19/kalkar1/thermal/data/ -p $P/paint -o $P/hough.csv -s $strategy -t $threshold -d 0.$maxvdev --max-workers $(nproc) > $P/log.txt
+            ./stitch/hough.py data/processed/dec19/kalkar1/thermal/data/ -p $P/hough -o $P/hough.csv -s $strategy -t $threshold -d 0.$maxvdev --max-workers $(nproc) > $P/log.txt
 
         done
     done
