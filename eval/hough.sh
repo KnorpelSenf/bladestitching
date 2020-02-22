676cf2aa014005bc7e900b4d97fea64c70a8a571
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-22 14:59:02 +0100

    Add grid search bash script

---
+++ eval/hough.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for strategy in {center,nub}; do
+    mkdir data/processed/dec19/kalkar1/thermal/eval/$strategy
+
+    for threshold in {50..250..10}; do
+        mkdir data/processed/dec19/kalkar1/thermal/eval/$strategy/$threshold
+
+        for maxvdev in {1..8}; do
+            P=data/processed/dec19/kalkar1/thermal/eval/$strategy/$threshold/$maxvdev
+            mkdir $P $P/hough
+
+            ./stitch/hough.py data/processed/dec19/kalkar1/thermal/data/ -p $P/paint -o $P/hough.csv -s $strategy -t $threshold -d $maxvdev &
+
+        done
+    done
+done
