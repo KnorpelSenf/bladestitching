f376c35ee276396141e42d7dd617b1e231949a93
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-24 10:44:16 +0100

    Allow arbitrary rotation angles

--- post/rotate.py
+++ post/rotate.py
@@ -2,21 +2,27 @@
 # -*- coding: utf8 -*-
 
 import cv2 as cv
+import numpy as np
 import os
 
 
-def rotate(imagefile, output):
+def rotate(imagefile, angle, output):
     img = cv.imread(imagefile)
-    img = cv.rotate(img, rotateCode=cv.ROTATE_90_CLOCKWISE)
-    cv.imwrite(output, img)
-    print('Rotated', imagefile, 'to', os.path.abspath(output))
+
+    img_center = tuple(np.array(img.shape[1::-1]) / 2)
+    rot_mat = cv.getRotationMatrix2D(img_center, angle, 1)
+    result = cv.warpAffine(
+        img, rot_mat, img.shape[1::-1], flags=cv.INTER_LINEAR)
+
+    cv.imwrite(output, result)
+    print('Rotated', imagefile, 'by', angle, 'to', os.path.abspath(output))
 
 
-def rotate_all(imagedir, output):
+def rotate_all(imagedir, angle, output):
     for file in sorted(os.listdir(imagedir)):
         imagefile = os.path.join(imagedir, file)
         outputfile = os.path.join(output, file)
-        rotate(imagefile, outputfile)
+        rotate(imagefile, angle, outputfile)
 
 
 if __name__ == '__main__':
@@ -26,6 +32,8 @@ if __name__ == '__main__':
 
     parser.add_argument('input', help='Image file or directory')
     parser.add_argument('-o', '--output', help='Output file or directory')
+    parser.add_argument('-a', '--angle', type=float, default=-90,
+                        help='Rotation angle, positive values mean counter-clockwise')
     args = parser.parse_args()
 
     is_dir = os.path.isdir(args.input)
@@ -40,6 +48,6 @@ if __name__ == '__main__':
             args.output = filename + '_rotated' + ext
 
     if is_dir:
-        rotate_all(args.input, args.output)
+        rotate_all(args.input, args.angle, args.output)
     else:
-        rotate(args.input, args.output)
+        rotate(args.input, args.angle, args.output)
