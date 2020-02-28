307c70581f519b837e1f0cef7674e754c085871d
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-28 14:08:37 +0100

    Better dir creation

--- eval/hough.sh
+++ eval/hough.sh
@@ -2,17 +2,11 @@
 
 BASE=data/processed/dec19/kalkar1/thermal
 
-mkdir $BASE/eval $BASE/eval/hough
-
 for strategy in {center,nub}; do
-    mkdir $BASE/eval/hough/$strategy
-
     for threshold in {50..250..10}; do
-        mkdir $BASE/eval/hough/$strategy/$threshold
-
         for maxvdev in {1..8}; do
             P=$BASE/eval/hough/$strategy/$threshold/$maxvdev
-            mkdir $P $P/hough
+            mkdir -p $P/hough
 
             echo "Strategy: $strategy | Threshold: $threshold | Max dev: 0.$maxvdev >>> $P"
             ./stitch/hough.py $BASE/data/ -p $P/hough -o $P/hough.csv -s $strategy -t $threshold -d 0.$maxvdev --max-workers $(nproc) > $P/log.txt
--- eval/sift.sh
+++ eval/sift.sh
@@ -2,17 +2,11 @@
 
 BASE=data/processed/dec19/kalkar1/thermal
 
-mkdir $BASE/eval/sift
-
 for count in {0,10,20,50,100,200,500,1000,2000,5000,10000}; do
-    mkdir $BASE/eval/sift/$count
-
     for ct in {1..9}; do
-        mkdir $BASE/eval/sift/$count/$ct
-
         for et in {2..18..2}; do
             P=$BASE/eval/sift/$count/$ct/$et
-            mkdir $P $P/sift
+            mkdir -p $P/sift
 
             echo "Count: $count | CT: 0.0$ct | ET: $et >>> $P"
             ./unused/sift.py $BASE/data/ -o $P/sift -c $count --contrast-threshold 0.0$ct --edge-threshold $et > $P/log.txt
