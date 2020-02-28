ccf143925b7c66f710dcac7092ff11e5017206fb
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-02-28 14:01:42 +0100

    Set gftt min dist to 5

--- eval/gftt.sh
+++ eval/gftt.sh
@@ -8,15 +8,11 @@ for count in {10,20,50,100,200,500,1000,5000}; do
     mkdir $BASE/eval/gftt/$count
 
     for quality in {0.005,0.01,0.015,0.02,0.025,0.03,0.035,0.04,0.045,0.05,0.055,0.06,0.065,0.07,0.08,,0.09,0.1,0.2,0.5}; do
-        mkdir $BASE/eval/gftt/$count/$quality
+        P=$BASE/eval/gftt/$count/$quality
+        mkdir $P $P/gftt
 
-        for distance in {5..50..5}; do
-            P=$BASE/eval/gftt/$count/$quality/$distance
-            mkdir $P $P/gftt
+        echo "Count: $count | Quality: $quality >>> $P"
+        ./unused/gftt.py $BASE/data/ -o $P/gftt -c $count -q $quality -d 5 >$P/log.txt
 
-            echo "Count: $count | Quality: $quality | Distance: $distance >>> $P"
-            ./unused/gftt.py $BASE/data/ -o $P/gftt -c $count -q $quality -d $distance > $P/log.txt
-
-        done
     done
 done
