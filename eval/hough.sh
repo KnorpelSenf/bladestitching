31054a472ea742ad5fca77ccf97c63be6aa0e632
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-03-05 12:13:30 +0100

    Always use center strategy

--- eval/hough.sh
+++ eval/hough.sh
@@ -2,15 +2,13 @@
 
 BASE=data/processed/dec19/kalkar1/thermal
 
-for strategy in {center,nub}; do
-    for threshold in {50..250..10}; do
-        for maxvdev in {1..8}; do
-            P=$BASE/eval/hough/$strategy/$threshold/$maxvdev
-            mkdir -p $P/hough
+for threshold in {50..250..10}; do
+    for maxvdev in {1..8}; do
+        P=$BASE/eval/hough/$threshold/$maxvdev
+        mkdir -p $P/hough
 
-            echo "Strategy: $strategy | Threshold: $threshold | Max dev: 0.$maxvdev >>> $P"
-            ./stitch/hough.py $BASE/data/ -p $P/hough -o $P/hough.csv -s $strategy -t $threshold -d 0.$maxvdev --max-workers $(nproc) > $P/log.txt
+        echo "Threshold: $threshold | Max dev: 0.$maxvdev >>> $P"
+        ./stitch/hough.py $BASE/data/ -p $P/hough -o $P/hough.csv -s center -t $threshold -d 0.$maxvdev --max-workers $(nproc) > $P/log.txt
 
-        done
     done
 done
