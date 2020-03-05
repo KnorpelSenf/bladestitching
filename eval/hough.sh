a626dc63ae65509bc9c8a74839ceae0dc8483f01
Steffen Trog <stu204766@mail.uni-kiel.de>
2020-03-05 13:03:03 +0100

    Added accumulating comment

--- eval/hough.sh
+++ eval/hough.sh
@@ -2,6 +2,10 @@
 
 BASE=data/processed/dec19/kalkar1/thermal
 
+# Note that you can use
+# for l in $(ls */*/log.txt);do echo $l,$(cat $l|grep "merely 0"|wc -l),$(cat $l|grep "merely 1"|wc -l),$(cat $l|grep "multiple" | wc -l);done
+# in order to accumulate the results into a csv file (filename,zero,one,multiple) when the WD is in $BASE/eval/hough
+
 for threshold in {50..250..10}; do
     for maxvdev in {1..8}; do
         P=$BASE/eval/hough/$threshold/$maxvdev
